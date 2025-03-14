adddca4c55e87fb82078f0fee5a1f10b52c46dde
Joshua Pridgen <joshuapridgen@u.boisestate.edu>
2025-03-13 22:36:33 -0600

    All finished with the project

--- README.md
+++ README.md
@@ -2,23 +2,34 @@
 
 ## Overview
 
-TODO: Write a project description
+This project is a simple interactive game where colorful balls bounce around the screen. The player controls an "Evil Circle" that can eliminate the balls by colliding with them. The game keeps track of the number of balls remaining on the screen and displays this count in real-time. The objective is to eliminate all the balls using the Evil Circle.
+
+## How to play
+
+I made two versions that you can play! If you find the commented code in `main.js`, you can switch between the keyboard and mouse control versions of the Evil Circle.
+
+### Keyboard Control Version
+
+1. Open `main.js`.
+2. Find the commented code block under the `EvilCircle` class constructor.
+3. Uncomment the lines that handle the keyboard events (`window.addEventListener('keydown', ...)`).
+4. Comment out or remove the `window.addEventListener('mousemove', ...)` line.
+
+### Mouse Control Version
+
+1. Open `main.js`.
+2. Find the commented code block under the `EvilCircle` class constructor.
+3. Ensure the `window.addEventListener('mousemove', ...)` line is uncommented.
+4. Comment out or remove the lines that handle the keyboard events (`window.addEventListener('keydown', ...)`).
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
-
-A simple bulleted list below is sufficient. For example:
-
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+- [MDN Web Docs - Canvas API](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+- [MDN Web Docs - Math.random()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+- [JavaScript.info - Animation](https://javascript.info/animation)
+- [Eloquent JavaScript - Drawing on Canvas](https://eloquentjavascript.net/17_canvas.html)
+- [HTML5 Rocks - High Performance Animations](http://www.html5rocks.com/en/tutorials/speed/high-performance-animations/)
+- [Stack Overflow - How to draw a circle with HTML5 canvas?](https://stackoverflow.com/questions/1255512/how-to-draw-a-circle-with-html5-canvas)
+- [Stack Overflow - How to detect collision in JavaScript?](https://stackoverflow.com/questions/2459298/how-to-detect-collision-in-javascript)
+- [Stack Overflow - How to move an object with arrow keys in JavaScript?](https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript)
+- [Stack Overflow - How to make an object follow the mouse in JavaScript?](https://stackoverflow.com/questions/7790725/javascript-track-mouse-position)
--- js/main.js
+++ js/main.js
@@ -18,14 +18,21 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -58,13 +65,88 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (this.exists && ball.exists && !(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+    // Keyboard version
+    //  keys a - left
+    //  keys d - right
+    //  keys w - up
+    //  keys s - down
+    // window.addEventListener('keydown', (e) => {
+    //   switch (e.key) {
+    //     case 'a':
+    //       this.x -= this.velX;
+    //       break;
+    //     case 'd':
+    //       this.x += this.velX;
+    //       break;
+    //     case 'w':
+    //       this.y -= this.velY;
+    //       break;
+    //     case 's':
+    //       this.y += this.velY;
+    //       break;
+    //   }
+    // });
+    
+    // Mouse version
+    window.addEventListener('mousemove', (e) => {
+      this.x = e.clientX;
+      this.y = e.clientY;
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x = width - this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x = this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y = height - this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y = this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
         }
       }
     }
@@ -89,15 +171,32 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilCircle = new EvilCircle(random(0, width), random(0, height));
+
+const ballCountElement = document.getElementById('ball-count');
+
+function updateBallCount() {
+  const count = balls.filter(ball => ball.exists).length;
+  ballCountElement.textContent = count;
+}
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
+  updateBallCount();
 
   requestAnimationFrame(loop);
 }
